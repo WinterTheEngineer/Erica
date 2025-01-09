aa489c5b8cab528c513f4efdbd8e04c6c5cf7e08
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2025-01-09 13:16:14 +0200

    created list creation view (new_list) and added validation

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,44 @@
-from django.shortcuts import render
+from .forms import ListForm
+from .models import List, ListItem
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def lists(request):
 
     context = {
-        'sector': "Lists"
+        'sector': "Lists",
+        'listform': ListForm
     }
 
     return render(request, 'lists.html', context)
+
+
+def new_list(request):
+
+    if request.method == "POST":
+        form = ListForm(request.POST)
+
+        if form.is_valid():
+            # Create List object first
+            new_list = List(
+                title=form.cleaned_data["title"],
+                description=form.cleaned_data["description"],
+                ordered=form.cleaned_data["ordered"]
+            )
+            new_list.save()
+
+            list_items = form.cleaned_data["listItems"].split(", ")
+
+            for list_item in list_items:
+                new_list_item = ListItem(name=list_item)
+                new_list_item.save()
+                new_list.listItems.add(new_list_item)
+            
+            # Finally, save the list
+            
+        else:
+            print(form.errors)
+    else:
+        form = ListForm()
+    
+    return redirect('lists')
