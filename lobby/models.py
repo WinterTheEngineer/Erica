37d6c26b8b7090426c1ba16ce6c15aa7d0cfea12
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2025-01-09 10:59:27 +0200

    refactored List and ListItem model objects from the lobby app to Lists

--- lobby/models.py
+++ lobby/models.py
@@ -1,21 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class ListItem(models.Model):
-
-    name = models.CharField(max_length=120)
-
-    def __str__(self):
-        return self.name
-
-
-class List(models.Model):
-
-    title = models.CharField(max_length=120)
-    description = models.CharField(max_length=255)
-    listItems = models.ManyToManyField(ListItem, blank=True)
-    
-    ordered = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.title
