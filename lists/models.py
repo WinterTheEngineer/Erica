f0de00039cb664b2d3560ee4ee9b31f02a24496f
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2025-01-09 13:01:53 +0200

    added creation and update timestamps to list models

--- lists/models.py
+++ lists/models.py
@@ -12,10 +12,11 @@ class ListItem(models.Model):
 class List(models.Model):
 
     title = models.CharField(max_length=120)
-    description = models.CharField(max_length=255, blank=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
     listItems = models.ManyToManyField(ListItem, blank=True)
-    
     ordered = models.BooleanField(default=False)
+    created = models.DateField(auto_now_add=True)
+    last_updated = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.title
