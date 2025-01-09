b700fae3ccb68ce8d9886a04fce8078479823e2e
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2025-01-09 13:15:27 +0200

    temporarily disabled ModelForm, created unbound form

--- lists/forms.py
+++ lists/forms.py
@@ -1,13 +1,26 @@
+from django import forms
 from .models import List, ListItem
-from django.forms import Form, ModelForm
 from django.forms import Textarea, CheckboxInput
+from django.utils.translation import gettext_lazy as _
 
 
-class ListForm(ModelForm):
-    class Meta:
-        fields = "__all__"
+# class ListForm(ModelForm):
+#     class Meta:
+#         model = List
+#         fields = "__all__"
 
-        widgets = {
-            "listItems": Textarea(),
-            "ordered": CheckboxInput()
-        }
+#         widgets = {
+#             "listItems": Textarea(),
+#             "ordered": CheckboxInput()
+#         }
+
+#         labels = {
+#             "listItems": _("List Items")
+#         }
+
+class ListForm(forms.Form):
+
+    title = forms.CharField(required=True)
+    description = forms.CharField(required=False)
+    listItems = forms.CharField(widget=Textarea, required=False)
+    ordered = forms.BooleanField(widget=CheckboxInput)
