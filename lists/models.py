e119d3943c81275e1036359870d8447ec31f0357
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2025-01-28 22:51:56 +0200

    modified relationship betwen List and List

--- lists/models.py
+++ lists/models.py
@@ -1,22 +1,22 @@
 from django.db import models
 
 # Create your models here.
-class ListItem(models.Model):
-
-    name = models.CharField(max_length=120)
-
-    def __str__(self):
-        return self.name
-
 
 class List(models.Model):
 
     title = models.CharField(max_length=120)
     description = models.CharField(max_length=255, blank=True, null=True)
-    listItems = models.ManyToManyField(ListItem, blank=True)
     ordered = models.BooleanField(default=False)
-    created = models.DateField(auto_now_add=True)
-    last_updated = models.DateField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
+class ListItem(models.Model):
+    name = models.CharField(max_length=120)
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
