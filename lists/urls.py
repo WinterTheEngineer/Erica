b872b124f51dfd72267f1e85a4c79f828d5e4145
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2025-02-02 23:41:36 +0200

    added url for adding new list items

--- lists/urls.py
+++ lists/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', views.lists, name='lists'),
     path('new_list', views.new_list, name='new_list'),
+    path('new_list_item', views.new_list_item, name='new_list_item'),
 ]
