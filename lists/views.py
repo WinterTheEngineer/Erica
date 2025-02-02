10318f180a0d34cad7b32de99bfac0277bfe522a
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2025-02-02 23:44:55 +0200

    created view to add new list items to lists

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
-from .forms import ListForm
 from .models import List, ListItem
 from django.contrib import messages
+from .forms import ListForm, ListItemForm
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -11,7 +11,8 @@ def lists(request):
     context = {
         'lists': lists,
         'sector': "Lists",
-        'listform': ListForm
+        'listform': ListForm,
+        'listitemform': ListItemForm
     }
 
     return render(request, 'lists.html', context)
@@ -35,12 +36,38 @@ def new_list(request):
 
             for list_item in list_items:
                 new_list_item = ListItem(name=list_item, list=new_list)
-                new_list_item.save()           
+                new_list_item.save()
+
+            messages.success(request, "The list was created successfully.")         
             
         else:
+            print(form.errors)
             messages.error(request, "A form validation error prevented me from created the list.")
     else:
         form = ListForm()
     
-    messages.success(request, "The list was created successfully.")
+    return redirect('lists')
+
+
+def new_list_item(request):
+
+    if request.method == "POST":
+        form = ListItemForm(request.POST)
+
+        if form.is_valid():
+            list_id = request.POST.get("listID")
+            changed_list = List.objects.get(id=list_id)
+            list_items = form.cleaned_data["listItems"].split(", ")
+
+            for list_item in list_items:
+                new_list_item = ListItem(name=list_item, list=changed_list)
+                new_list_item.save()
+                
+                messages.success(request, "The list was amended successfully.")
+        else:
+            messages.error(request, "A form validation error prevented me from adding these list items.")
+    else:
+        form = ListItemForm()
+    
+    
     return redirect('lists')
