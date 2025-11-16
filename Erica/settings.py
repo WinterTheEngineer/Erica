e295c40e7a49a51e4dd55378b832ad42dc01d16a
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2025-11-16 02:05:12 +0200

    domain setup

--- Erica/settings.py
+++ Erica/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG').lower() == "true"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app',
+                 'ericaos.vercel.app']
 
 
 # Application definition
